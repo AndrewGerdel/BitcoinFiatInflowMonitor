3b99f849a7c6994b78ddb8d3419247651bbab9de
Andrew Gerdel <AndrewGerdel@yahoo.com>
2020-02-10 10:14:50 -0600

    Calculates average sum, displays buy/sell suggestion

--- start.ts
+++ start.ts
@@ -1,17 +1,46 @@
 var binance = require('./binance');
 var coinbase = require('./coinbasepro');
+var summedValues = [];
+const minCount = 180;
 
-var GetRates = (async() => {
+var GetRates = (async () => {
+    //Get orderbook totals from binance and coinbase
     let binanceResults = await binance.GetOrderbookTotals();
     let coinbaseResults = await coinbase.GetOrderbookTotals();
+
+    //Sum the bids and asks
     let sumBids = binanceResults.totalBidsUsd + coinbaseResults.totalBidsUsd;
     let sumAsks = binanceResults.totalAsksUsd + coinbaseResults.totalAsksUsd;
 
-    console.log(sumBids - sumAsks);
-    
+    //A negative summed value indicates there is sell pressure. Positive indicates there is buying pressure. 
+    let summedValue = sumBids - sumAsks;
+
+    //The numbers seem to bounce around a lot. So we will average the last 180 sums (30 minutes) to determine what the market is likely doing. 
+    //If we don't have 30 mins worth of data, just use whatever we have. 
+    summedValues.push(summedValue);
+    let averageSum = await GetSumAverage();
+
+    //Display the average. 
+    let direction = averageSum > 0 ? 'Buy': 'Sell';
+    console.log(`Average Sum: %s (%s)`, averageSum, direction);
+});
+
+var GetSumAverage = (async () => {
+    let totalAllSums = 0;
+    if (summedValues.length < minCount) {
+        summedValues.forEach((v) => {
+            totalAllSums += v;
+        });
+        return (totalAllSums / summedValues.length);
+    } else {
+        for (let i = summedValues.length - minCount; i < summedValues.length; i++) {
+            totalAllSums += summedValues[i]
+        }
+        return (totalAllSums / minCount);
+    }
 });
 
 GetRates();
 setInterval(() => {
     GetRates();
-}, 10000 )
+}, 10000)
